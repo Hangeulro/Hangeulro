49fb99a630352e670869e00458e0ef1c14ff7232
Junseok Oh <kotohana5706@edcan.kr>
2016-06-19 23:14:02 +0900

    Implement SplashActivity

--- app/src/main/java/kr/edcan/hangeulro/activity/SplashActivity.java
+++ app/src/main/java/kr/edcan/hangeulro/activity/SplashActivity.java
@@ -3,6 +3,8 @@ package kr.edcan.hangeulro.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.edcan.hangeulro.R;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
