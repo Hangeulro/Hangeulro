4d66cd85c9b783d9bdc3a36ba676639460eec40f
Junseok Oh <kotohana5706@edcan.kr>
2016-06-22 05:53:37 +0900

    add new drawable resource for service alert, ClipBoardService reformatted for Hangeulro

--- app/src/main/java/kr/edcan/hangeulro/utils/ClipBoardService.java
+++ app/src/main/java/kr/edcan/hangeulro/utils/ClipBoardService.java
@@ -21,6 +21,7 @@ import android.view.WindowManager;
 import java.util.Date;
 
 import kr.edcan.hangeulro.R;
+import kr.edcan.hangeulro.activity.MainActivity;
 
 
 /**
@@ -61,9 +62,9 @@ public class ClipBoardService extends Service {
         killbuild.addNextIntent(new Intent(getApplicationContext(), KillProcess.class));
         PendingIntent killProcess = killbuild.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_status_running)
-                .setContentTitle("Exchat")
-                .setContentText("Exchat 서비스가 실행중입니다.")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("한글을 한글로!")
+                .setContentText("퀵서치 기능이 작동중입니다.")
                 .addAction(R.drawable.btn_status_deleteapp, "종료", killProcess);
         startMain = new Intent(this, MainActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
@@ -79,10 +80,6 @@ public class ClipBoardService extends Service {
             Log.e("asdf", "fastSearch");
             startForeground(INTENT_KEY, builder.build());
         }
-        String[] s = {"", ""};
-        for (String asdf : s) {
-            Log.e("asf", asdf);
-        }
         manager.addPrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
             @Override
             public void onPrimaryClipChanged() {
@@ -91,13 +88,9 @@ public class ClipBoardService extends Service {
                     if (System.currentTimeMillis() - sharedPreferences.getLong("lastFastSearchTime", System.currentTimeMillis() - 201) > 200) {
                         if (manager.getPrimaryClipDescription().toString().contains("text")) {
                             String capturedString = manager.getPrimaryClip().getItemAt(0).getText().toString();
-                            ExchatClipboardUtils utils = new ExchatClipboardUtils(getApplicationContext());
-                            ClipBoardData clipData = utils.getResult(capturedString);
-                            if (clipData != null) {
-                                if (sharedPreferences.getBoolean("fastSearchVibrate", true)) {
+                            if (capturedString.contains("빼박캔트")) {
                                     Vibrator vb = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
                                     vb.vibrate(500);
-                                }
                                 materialDialog = new MaterialDialog.Builder(getBaseContext())
                                         .customView(getView(clipData), false)
                                         .theme(Theme.LIGHT)
@@ -106,18 +99,6 @@ public class ClipBoardService extends Service {
                                 WindowManager.LayoutParams wmlp = materialDialog.getWindow().getAttributes();
                                 wmlp.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
                                 materialDialog.show();
-
-                                Realm realm = Realm.getInstance(getApplicationContext());
-                                realm.beginTransaction();
-                                HistoryData data = realm.createObject(HistoryData.class);
-                                Date date = new Date(System.currentTimeMillis());
-                                data.setConvertUnit(0);
-                                data.setPrevUnit(clipData.getUnit());
-                                data.setConvertValue(clipData.getConvertValue());
-                                data.setPrevValue(clipData.getValue());
-                                data.setDate(date);
-                                //Log.e("asdf", "saved " + clipData.getConvertValue());
-                                realm.commitTransaction();
                                 editor.putLong("lastFastSearchTime", System.currentTimeMillis());
                                 editor.commit();
                             }
@@ -129,35 +110,9 @@ public class ClipBoardService extends Service {
         return START_NOT_STICKY;
     }
 
-    private View getView(ClipBoardData clipData) {
+    private View getView() {
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.activity_clipboard_popup_view, null);
-        final ExchatTextView prev, result, share, launch;
-        prev = (ExchatTextView) view.findViewById(R.id.dialog_prev);
-        result = (ExchatTextView) view.findViewById(R.id.dialog_result);
-        share = (ExchatTextView) view.findViewById(R.id.popup_share);
-        launch = (ExchatTextView) view.findViewById(R.id.popup_launch);
-        ExchatClipboardUtils utils = new ExchatClipboardUtils(getApplicationContext());
-        ClipBoardData data = new ClipBoardData(clipData.getUnit(), clipData.getValue(), clipData.getConvertValue());
-        prev.setText(data.getValue() + " " + utils.foreignmoneyUnits[data.getUnit()]);
-        result.setText(data.getConvertValue() + " " + "KRW");
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                sharingIntent.setType("text/plain");
-                sharingIntent.putExtra(Intent.EXTRA_TEXT, prev.getText().toString() + " = " + result.getText().toString() + "입니다. #Exchat.");
-                materialDialog.dismiss();
-                startActivity(Intent.createChooser(sharingIntent, "환율 정보 공유").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-            }
-        });
-        launch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                materialDialog.dismiss();
-                startActivity(startMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-            }
-        });
         return view;
     }
 
