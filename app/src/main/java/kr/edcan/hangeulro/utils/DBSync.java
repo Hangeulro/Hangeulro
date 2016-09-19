07b5f255072fa6b88344c31731f000d28a6cfe91
Junseok Oh <kotohana5706@edcan.kr>
2016-09-19 10:09:53 +0900

    [DBSync] Create DBSync to sync database

---
+++ app/src/main/java/kr/edcan/hangeulro/utils/DBSync.java
@@ -0,0 +1,19 @@
+package kr.edcan.hangeulro.utils;
+
+import android.content.Context;
+
+import io.realm.Realm;
+
+/**
+ * Created by JunseokOh on 2016. 9. 19..
+ */
+public class DBSync {
+    private Context context;
+    private Realm realm;
+
+    public DBSync(Context context) {
+        this.context = context;
+
+    }
+
+}
