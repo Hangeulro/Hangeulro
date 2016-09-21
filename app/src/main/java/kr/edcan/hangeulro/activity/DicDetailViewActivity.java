0f5cc4df84842f1375530b2f180b46d671c09251
Junseok Oh <kotohana5706@edcan.kr>
2016-09-22 05:37:26 +0900

    [DicDetailView] Implement DicDetailView

---
+++ app/src/main/java/kr/edcan/hangeulro/activity/DicDetailViewActivity.java
@@ -0,0 +1,13 @@
+package kr.edcan.hangeulro.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DicDetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dic_detail_view);
+    }
+}
