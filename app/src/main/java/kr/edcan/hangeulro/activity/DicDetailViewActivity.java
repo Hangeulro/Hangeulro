9e96d896f62565353b97abf81a359f0f5646313f
Junseok Oh <kotohana5706@edcan.kr>
2016-09-22 06:01:18 +0900

    [CartaTagView] Implement CartaTagView:

--- app/src/main/java/kr/edcan/hangeulro/activity/DicDetailViewActivity.java
+++ app/src/main/java/kr/edcan/hangeulro/activity/DicDetailViewActivity.java
@@ -3,6 +3,8 @@ package kr.edcan.hangeulro.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.edcan.hangeulro.R;
+
 public class DicDetailViewActivity extends AppCompatActivity {
 
     @Override
