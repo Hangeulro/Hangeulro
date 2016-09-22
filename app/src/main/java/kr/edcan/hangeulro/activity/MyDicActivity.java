3ad69964e7c84f382f4027c18627666a29a5f1b2
Junseok Oh <kotohana5706@edcan.kr>
2016-09-22 11:25:23 +0900

    [MyDic] Implement MyDicActivity

---
+++ app/src/main/java/kr/edcan/hangeulro/activity/MyDicActivity.java
@@ -0,0 +1,15 @@
+package kr.edcan.hangeulro.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.edcan.hangeulro.R;
+
+public class MyDicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_dic);
+    }
+}
