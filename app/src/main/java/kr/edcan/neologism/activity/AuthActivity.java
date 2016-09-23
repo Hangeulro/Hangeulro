c50d0c6634697e60d5961a511df210bd53fc62b4
Junseok Oh <kotohana5706@edcan.kr>
2016-09-24 01:16:01 +0900

    Configure Auth HTTP Interface

--- app/src/main/java/kr/edcan/neologism/activity/AuthActivity.java
+++ app/src/main/java/kr/edcan/neologism/activity/AuthActivity.java
@@ -1,15 +1,36 @@
 package kr.edcan.neologism.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import kr.edcan.neologism.R;
+import kr.edcan.neologism.databinding.ActivityAuthBinding;
 
 public class AuthActivity extends AppCompatActivity {
+
+    ActivityAuthBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_auth);
+        setDefault();
+    }
+
+    private void setDefault() {
+        binding.authIDLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginStr = binding.authIdInput.getText().toString().trim();
+                String passwordStr = binding.authPasswordInput.getText().toString().trim();
+                if (!loginStr.equals("") && !passwordStr.equals("")) {
+                    // launch login
+                } else Toast.makeText(AuthActivity.this, "빈칸 없이 입력해주세요!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/kr/edcan/neologism/utils/NetworkInterface.java
+++ app/src/main/java/kr/edcan/neologism/utils/NetworkInterface.java
@@ -4,12 +4,35 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * Created by JunseokOh on 2016. 9. 18..
  */
 public interface NetworkInterface {
+
+    @POST("/auth/login")
+    @FormUrlEncoded
+    Call<ResponseBody> userLogin(@Field("userid") String userid, @Field("pw") String password);
+
+    @POST("/auth/login/auto")
+    @FormUrlEncoded
+    Call<ResponseBody> userAutoLogin(@Field("id") String userid, @Field("token") String token);
+
+    @POST("/auth/login/register")
+    @FormUrlEncoded
+    Call<ResponseBody> userRegister(@Field("userid") String userid, @Field("pw") String password, @Field("username") String username);
+
+    @GET("/auth/fb/token")
+    Call<ResponseBody> facebookLogin(@Query("access_token") String accessToken);
+
+    @GET("/auth/tw/token")
+    Call<ResponseBody> twitterLogin(
+            @Query("oauth_token") String accessToken,
+            @Query("oauth_token_secret") String accessTokenSecret,
+            @Query("user_id") String userid);
 
     @POST("/word/cata")
     @FormUrlEncoded
