aca2d355e061cdb73f6d51ed674e1a834947baa8
Junseok Oh <kotohana5706@edcan.kr>
2016-09-23 16:22:58 +0900

    Implement AuthActivity

---
+++ app/src/main/java/kr/edcan/neologism/activity/AuthActivity.java
@@ -0,0 +1,13 @@
+gipackage kr.edcan.neologism.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
