5d3e0d356c015f342404a713dd8faf6deb173331
Junseok Oh <kotohana5706@edcan.kr>
2016-09-24 12:39:17 +0900

    [Quiz] Implement Activities used for Quiz

---
+++ app/src/main/java/kr/edcan/neologism/activity/QuizResultActivity.java
@@ -0,0 +1,15 @@
+package kr.edcan.neologism.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.edcan.neologism.R;
+
+public class QuizResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_result);
+    }
+}
