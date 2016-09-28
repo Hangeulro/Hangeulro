b6b32d1db9096ae9a663dc45896026287b598713
Junseok Oh <kotohana5706@edcan.kr>
2016-09-28 22:25:32 +0900

    [MyDic] Configure Data Model

--- app/src/main/java/kr/edcan/neologism/model/MyDic.java
+++ app/src/main/java/kr/edcan/neologism/model/MyDic.java
@@ -1,7 +1,34 @@
 package kr.edcan.neologism.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by JunseokOh on 2016. 9. 28..
  */
 public class MyDic {
+    public String owner, dicname, sub;
+    public ArrayList<String> favorite;
+
+    public MyDic(String owner, String dicname, String sub, ArrayList<String> favorite) {
+        this.owner = owner;
+        this.dicname = dicname;
+        this.sub = sub;
+        this.favorite = favorite;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getDicname() {
+        return dicname;
+    }
+
+    public String getSub() {
+        return sub;
+    }
+
+    public ArrayList<String> getFavorite() {
+        return favorite;
+    }
 }
