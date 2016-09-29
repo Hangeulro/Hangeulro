50fb35065515b462c8e3b712c6acaecdd614acb2
Junseok Oh <kotohana5706@edcan.kr>
2016-09-29 23:21:37 +0900

    [NeologismBoard] Implement Activity

--- app/src/main/java/kr/edcan/neologism/activity/NeologismBoardActivity.java
+++ app/src/main/java/kr/edcan/neologism/activity/NeologismBoardActivity.java
@@ -3,6 +3,8 @@ package kr.edcan.neologism.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.edcan.neologism.R;
+
 public class NeologismBoardActivity extends AppCompatActivity {
 
     @Override
